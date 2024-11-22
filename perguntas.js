082bb06ee814911af7a075fd3526129c769520cf
annac2000 <00001114116464sp@al.educacao.sp.gov.br>
2024-11-22 16:08:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,72 @@
+criaCartao(
+    'conhecimentos gerais', 
+    'Qual era o nome do chefe do crime que liderava a Chicago Outfit (Máfia de Chicago)?',
+  ' Al Capone'
+)
+
+criaCartao(
+    'conhecimentos gerais', 
+    'Quem era o antigo Deus Grego do Sol?',
+    ' Apolo'
+)
+
+criaCartao(
+    'conhecimentos gerais', 
+    'Qual é o sobrenome mais comum nos Estados Unidos?',
+    'Smith'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    ' Que doença comumente se espalha em navios piratas?',
+    'Escorbuto'
+)
+
+criaCartao(
+    'conhecimentos gerais', 
+    'Em que cidade você estaria se estivesse nas Escadarias da Praça da Espanha? ',
+    'Roma'
+)
+
+    criaCartao(
+        'conhecimentos gerais', 
+        'Qual cidade tem a maior expectativa de vida no mundo?',
+        'Hong Kong'
+    )               
+
+    criaCartao(
+        'conhecimentos gerais', 
+        'Em que ano foi criada a ONU? ',
+        '1945'
+    )
+
+    criaCartao(
+        'conhecimentos gerais', 
+        'Qual empresa foi originalmente chamada de "Cadabra"?',
+        'Amazon'
+    )
+
+    criaCartao(
+        'conhecimentos gerais', 
+        'Quantos elementos existem na tabela periódica? ',
+        '118'
+    )
+
+    criaCartao(
+        'conhecimentos gerais', 
+        'Qual fabricante de automóveis teve a maior receita em 2020?',
+        'Volkswagen'
+    )
+
+    criaCartao(
+        'conhecimentos gerais', 
+        'Quantos minutos tem uma semana inteira?',
+        '10.080'
+    )
+
+    criaCartao(
+        'conhecimentos gerais', 
+        'Quem ganhou o maior número total de Óscares',
+        'Walt Disney'
+    )
+
